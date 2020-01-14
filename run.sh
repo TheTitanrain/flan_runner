f4c288e2720a3a2f5aedf2bac2132282626b6b63
Alex <1952721+TheTitanrain@users.noreply.github.com>
2020-01-14 10:40:37 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-
+cd /home/titanrain/
+sudo rm -rf flan
+sudo git clone https://github.com/cloudflare/flan
+cd flan/
+sudo chmod 777 shared/ips.txt
+sudo echo 212.19.20.180 > shared/ips.txt
+sudo make build
+current_time=$(date "+%Y.%m.%d-%H.%M")
+sudo make start
+sudo proxychains4 curl -F chat_id=115263918 -F document=@shared/reports/report_$current_time.tex https://api.telegram.org/bot913046036:AAGOjWdKibTzxwzjLQ_1huoYFF_1MTBWUJA/sendDocument
